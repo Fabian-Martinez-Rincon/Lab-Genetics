e8ede52d37eeaccae8b88a878db5d31d2b44ff2a
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-10-14 07:42:27 -0300

    acomodando el init

--- src/web/controllers/__init__.py
+++ src/web/controllers/__init__.py
@@ -1,8 +1,20 @@
 import flask
-from src.web.controllers import root, registrar, listar_usuarios, listar_laboratorios, registrar_laboratorio, editar_perfil
+from src.web.controllers import (
+    root,
+    registrar,
+    listar_usuarios,
+    listar_laboratorios,
+    registrar_laboratorio,
+    editar_perfil
+)
 
 _blueprints = (
-    root.bp, registrar.bp, listar_usuarios.bp, listar_laboratorios.bp, registrar_laboratorio.bp, editar_perfil.bp
+    root.bp,
+    registrar.bp,
+    listar_usuarios.bp,
+    listar_laboratorios.bp,
+    registrar_laboratorio.bp,
+    editar_perfil.bp
 )
 
 def init_app(app: flask.Flask):
