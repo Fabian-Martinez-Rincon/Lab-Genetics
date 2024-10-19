f82b581faab0bb3e8b33ab05be7c36d8e9e64b79
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-10-18 23:50:56 -0300

    dejando al paciente que puede ser null en un turno

--- src/core/models/turno.py
+++ src/core/models/turno.py
@@ -3,7 +3,7 @@ from src.core.models.database import db
 class Turno(db.Model):
     __tablename__="turnos"
     id = db.Column(db.Integer, primary_key=True, unique= True)
-    id_paciente = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
+    id_paciente = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=True)  # Permite NULL
     id_laboratorio = db.Column(db.Integer, db.ForeignKey("laboratorios.id"), nullable=False)
     fecha = db.Column(db.Date, nullable=False)
     hora = db.Column(db.Time, nullable=False)
