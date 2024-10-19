24f68a490e145e6ee1474a394b4f802cb43adb7b
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-10-19 01:05:09 -0300

    separando las funcionalidades

--- src/web/controllers/utils.py
+++ src/web/controllers/utils.py
@@ -1,11 +1,25 @@
 from flask import session, flash, redirect, url_for
-from functools import wraps 
+from functools import wraps
 from src.core.models.usuario import Usuario
+
+def verificar_autenticacion(f):
+    """
+    Decorador que verifica si el usuario ha iniciado sesión.
+    Si no está autenticado, redirige al usuario a la página principal.
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        user_id = session.get('user_id')
+        if not user_id:
+            flash('Debes iniciar sesión para realizar esta operación.', 'error')
+            return redirect(url_for('root.index_get'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 def verificar_rol(rol_permitido):
     """
-    Decorador que restringe el acceso a una ruta según el rol del usuario autenticado.
-
+    Decorador que verifica si el usuario autenticado tiene el rol adecuado.
+    
     Parámetros:
     rol_permitido (int): El rol permitido para acceder a la ruta.
 
@@ -17,29 +31,15 @@ def verificar_rol(rol_permitido):
     - 5: Paciente
     - 6: Transportista
 
-    Si el usuario no está autenticado o su rol no coincide con el rol permitido,
-    se redirige al usuario a la página principal y se muestra un mensaje de error.
-
-    Uso:
-    @verificar_rol(2)
-    def listar_turnos():
-        # Código de la vista
-        pass
-
-    En este ejemplo, solo los usuarios con el rol de "Administrador" (rol 2)
-    pueden acceder a la ruta decorada.
+    Si el rol del usuario no coincide con el rol permitido, redirige
+    al usuario a la página principal y muestra un mensaje de error.
     """
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            # Verifica si el usuario ha iniciado sesión
             user_id = session.get('user_id')
-            if not user_id:
-                flash('Debes iniciar sesión para realizar esta operación.', 'error')
-                return redirect(url_for('root.index_get'))
-
-            # Obtén el rol del usuario actual
             usuario = Usuario.query.get(user_id)
+
             if not usuario or usuario.id_rol != rol_permitido:
                 flash('No tienes permiso para realizar esta operación.', 'error')
                 return redirect(url_for('root.index_get'))
