630c3b98dce758365d947b3403bd1effb0278b60
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-11-14 22:44:56 -0300

    update seed agregando un historial de estados

--- src/core/seed.py
+++ src/core/seed.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 from src.core.models.database import db
 from src.core.models import Laboratorio, Usuario, Rol, Turno, Estado, Resultado, Estudio, Presupuesto, Pedido, Patologia
-
+from src.core.models import HistorialEstado
 
 def cargar_datos(filename):
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -44,13 +44,22 @@ def seed_db():
             #Turno: 'turnos.json',
             Patologia: 'patologias.json'
         }
-        
+
         for entidad, archivo in entidades_datos.items():
             datos = cargar_datos(archivo)
             eliminar_y_agregar(entidad, datos)
+
+        historial_datos = cargar_datos('historial_estados.json')
+        eliminar_y_agregar_historial(HistorialEstado, historial_datos)
 
         db.session.commit()
     except Exception as e:
         db.session.rollback()
         print(f"Error seeding database: {e}")
 
+def eliminar_y_agregar_historial(entidad, datos):
+    db.session.query(entidad).delete()
+    for data in datos:
+        # Convertir `fecha_hora` a datetime
+        data['fecha_hora'] = datetime.fromisoformat(data['fecha_hora'])
+        db.session.add(entidad(**data))
