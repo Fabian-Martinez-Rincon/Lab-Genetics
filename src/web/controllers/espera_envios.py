d8ef8ec1a043996b89d2974d6db4e3c13fb8f205
MateoTaru <mateotaru@gmail.com>
2024-11-28 22:39:58 -0300

    update espera_envios actualizando

--- src/web/controllers/espera_envios.py
+++ src/web/controllers/espera_envios.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from src.web.controllers.utils import verificar_autenticacion, verificar_rol
+from src.web.controllers.utils import verificar_autenticacion, verificar_rol, enviar_estudios_automaticamente
 from src.core.models.estudio import Estudio
 from src.core.models.database import db
 from src.core.models.historialEstado import HistorialEstado
@@ -9,6 +9,8 @@ from src.core.models.notificacion import Notificacion
 from src.core.models.exterior import Exterior
 
 bp = Blueprint("espera_envios", __name__)
+
+@enviar_estudios_automaticamente
 @verificar_autenticacion
 @verificar_rol(2)
 @bp.route('/espera_envios', methods=['GET','POST'])
@@ -18,27 +20,4 @@ def espera_envios():
     .filter(HistorialEstado.estado == "ESPERANDO ENVIO AL EXTERIOR") \
     .all()
     cantidad_estudios = len(estudios)
-    if request.method == 'POST':
-        primeros_estudios = Estudio.query \
-        .join(HistorialEstado, Estudio.id == HistorialEstado.estudio_id) \
-        .filter(HistorialEstado.estado == "ESPERANDO ENVIO AL EXTERIOR") \
-        .filter(Estudio.fecha_ingreso_central != None) \
-        .order_by(Estudio.fecha_ingreso_central.asc()) \
-        .limit(100) \
-        .all()
-        if not primeros_estudios or len(primeros_estudios) != 100:
-            flash('No hay estudios para enviar al exterior. Solo Se pueden enviar de a 100', 'error')
-            return redirect(url_for('espera_envios.espera_envios'))
-        exterior = Exterior(
-            estado="ENVIADO AL EXTERIOR"
-        )
-        db.session.add(exterior)
-        for estudio in primeros_estudios:
-            estudio.historial.append(HistorialEstado(estado="ENVIADO AL EXTERIOR"))
-            Notificacion.send_mail(estudio.id_paciente, f"Su estudio {estudio.id} ha sido enviado al exterior.")
-            exterior.estudios.append(estudio)
-        db.session.commit()
-        flash('Estudios envios exitosamente.', 'success')
-        return redirect(url_for('espera_envios.espera_envios'))
-    
     return render_template('administrador/espera_envios.html', estudios=estudios, cantidad_estudios=cantidad_estudios)
